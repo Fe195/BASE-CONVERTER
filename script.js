177b069200a6e57f63229dc722fe13eb5feb2712
Feba Rachel Lalu <114395573+Fe195@users.noreply.github.com>
2023-01-03 11:40:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,190 +1,104 @@
-const baseFrom= document.querySelector("#base-from");
-const baseTo= document.querySelector("#base-to");
-const input= document.querySelector("#txtNumber");
-const result= document.querySelector("#result");
-const swap= document.querySelector("#btnSwap");
-const convert= document.querySelector("#btnConvert");
-const checkBinary= /^[0-1]+$/;
-const checkDecimal= /^[0-9]+$/;
-const checkOctal= /^[0-7]+$/;
-const checkHexadecimal= /^(?!-)(?!.*-)[A-Ea-e0-9-]+(?<!-)$/;
+let input_unit = document.getElementById('unit');
+let output_unit = document.getElementById('changed-unit');
+let input_text = document.getElementById('text');
+let output_text = document.getElementById('changed-text');
+let swapBtn = document.getElementById('swap');
+let convertBtn = document.getElementById('convert');
 
 
-convert.addEventListener("click",function(){
-    const baseFrom= document.querySelector("#base-from");
-    const baseTo= document.querySelector("#base-to");
+swapBtn.addEventListener('click', swap);
+convertBtn.addEventListener('click', convert);
 
 
-
-    const convertFrom = baseFrom.value;
-    // console.log(convertFrom);
-    const convertTo = baseTo.value;
-    // console.log(convertTo)
-    // console.log(input.value);
+function swap() {
 
 
-    if(input.value==""){
-        result.value="Please Enter Input";
-    }
-    else {
+    let temp = input_unit.value;
+    input_unit.value = output_unit.value;
+    output_unit.value = temp;
 
-        
-        if(convertFrom == "from-binary" && convertTo == "to-decimal"){
-            if(checkBinary.test(input.value)){
-                result.value = parseInt(input.value,2);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }
+    let num = input_text.value;
+    input_text.value = output_text.value;
+    output_text.value = num;
+
+    convert();
+
+}
+
+
+function convert() {
+    let input_value = input_unit.value;
+    let output_value = output_unit.value;
+
+    if (input_value == "bin") {
+        let binVal = input_text.value;
+
+        if (output_value == "dec") {
+            output_text.value = parseInt(binVal, 2).toString();
         }
-        else if(convertFrom == "from-binary" && convertTo == "to-hexadecimal"){
-            if(checkBinary.test(input.value)){
-                result.value = parseInt(input.value,2).toString(16).toUpperCase();
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }       
+        else if (output_value == "octa") {
+            output_text.value = parseInt(binVal, 2).toString(8).toUpperCase();
         }
-        else if(convertFrom == "from-binary" && convertTo == "to-octal"){
-            if(checkBinary.test(input.value)){
-                result.value = parseInt(input.value,2).toString(8);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }       
+        else if (output_value == "hexa") {
+            output_text.value = parseInt(binVal, 2).toString(16).toUpperCase();
         }
-        else if(convertFrom == "from-binary" && convertTo == "to-binary"){
-            if(checkBinary.test(input.value)){
-                result.value = input.value;
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }             
-        }
-        else if(convertFrom == "from-decimal" && convertTo == "to-binary"){
-            if(checkDecimal.test(input.value)){
-                result.value = parseInt(input.value).toString(2);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }             
-        }
-        else if(convertFrom == "from-decimal" && convertTo == "to-hexadecimal"){
-            if(checkDecimal.test(input.value)){
-                result.value = parseInt(input.value).toString(16).toUpperCase();
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }                 
-        }
-        else if(convertFrom == "from-decimal" && convertTo == "to-octal"){
-            if(checkDecimal.test(input.value)){
-                result.value = parseInt(input.value).toString(8);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }          
-        }
-        else if(convertFrom == "from-decimal" && convertTo == "to-decimal"){
-            if(checkDecimal.test(input.value)){
-                result.value = input.value;
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }          
-        }
-        else if(convertFrom == "from-octal" && convertTo == "to-decimal"){
-            if(checkOctal.test(input.value)){
-                result.value = parseInt(input.value,8).toString(10);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }               
-        }
-        else if(convertFrom == "from-octal" && convertTo == "to-binary"){
-            if(checkOctal.test(input.value)){
-                result.value = parseInt(input.value,8).toString(2);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }      
-        }
-        else if(convertFrom == "from-octal" && convertTo == "to-hexadecimal"){
-            if(checkOctal.test(input.value)){
-                result.value = parseInt(input.value,8).toString(16).toUpperCase();
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }       
-        }
-        else if(convertFrom == "from-octal" && convertTo == "to-octal"){
-            if(checkOctal.test(input.value)){
-                result.value = input.value;
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }         
-        }
-        else if(convertFrom == "from-hexadecimal" && convertTo == "to-decimal"){ 
-            if(checkHexadecimal.test(input.value)){
-                result.value = parseInt(input.value,16).toString(10);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }         
-        }
-        else if(convertFrom == "from-hexadecimal" && convertTo == "to-octal"){ 
-            if(checkHexadecimal.test(input.value)){
-                result.value = parseInt(input.value,16).toString(8);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }       
-        }
-        else if(convertFrom == "from-hexadecimal" && convertTo == "to-binary"){
-            if(checkHexadecimal.test(input.value)){
-                result.value = parseInt(input.value,16).toString(2);
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }      
-        }
-        else if(convertFrom == "from-hexadecimal" && convertTo == "to-hexadecimal"){
-            
-            if(checkHexadecimal.test(input.value)){
-                result.value = input.value.toUpperCase();
-            }
-            else{
-                result.value="Please Enter Valid Input";
-            }      
+        else if (output_value == "bin") {
+            output_text.value = binVal;
         }
     }
 
-})
+    if (input_value == "dec") {
+        let decVal = input_text.value;
 
-// swap.addEventListener("click",function(){
-   
-   
-  
-// 	const temp=baseFrom.value;
-//     const temp2=baseTo.value;
-//     console.log(temp+"asd");
-//     console.log(temp2+"lkj");
-// 	baseTo.value=temp;
-// 	baseFrom.value=temp2;
-//     console.log(baseFrom.options)
-//     console.log(baseTo.options)
+        if (output_value == "bin") {
+            output_text.value = parseInt(decVal, 10).toString(2);
+        }
+        else if (output_value == "octa") {
+            output_text.value = parseInt(decVal, 10).toString(8).toUpperCase();
+        }
+        else if (output_value == "hexa") {
+            output_text.value = parseInt(decVal, 10).toString(16).toUpperCase();
+        }
+        else if (output_value == "dec") {
+            output_text.value = decVal;
+        }
+    }
 
 
+    if (input_value == "octa") {
+        let octaVal = input_text.value;
 
-//     let input= document.querySelector("#txtNumber");
-//     if (input.value !="") {
-//         input.value = "";
-//     }
-//     let result= document.querySelector("#result");
-//     if (result.value !="") {
-//         result.value = "";
-//     }
+        if (output_value == "bin") {
+            output_text.value = parseInt(octaVal, 8).toString(2);
+        }
+        else if (output_value == "dec") {
+            output_text.value = parseInt(octaVal, 8).toString(10).toUpperCase();
+        }
+        else if (output_value == "hexa") {
+            output_text.value = parseInt(octaVal, 8).toString(16).toUpperCase();
+        }
+        else if (output_value == "octa") {
+            output_text.value = octaVal;
+        }
+    }
 
-   
-// })
+
+    if (input_value == "hexa") {
+        let hexaVal = input_text.value;
+
+        if (output_value == "bin") {
+            output_text.value = parseInt(hexaVal, 16).toString(2);
+        }
+        else if (output_value == "dec") {
+            output_text.value = parseInt(hexaVal, 16).toString(10).toUpperCase();
+        }
+        else if (output_value == "octa") {
+            output_text.value = parseInt(hexaVal, 16).toString(8).toUpperCase();
+        }
+        else if (output_value == "hexa") {
+            output_text.value = hexaVal;
+        }
+    }
+
+
+}
